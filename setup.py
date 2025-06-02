6543391cc30f069cfc9b2acec0d598c9d8ad7108
Juan Durón Ramirez <juan.duron@seccionamarilla.com>
2025-06-02 17:00:43 -0600

    primer releases

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                      # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Juan Duron",                                    # Tu nombre
+    author_email="juan.duron@seccionamarilla.com",          # Tu correo electrónico
+    url="https://github.com/juanduronadsa/tercerRepo",      # URL del proyecto
+)
